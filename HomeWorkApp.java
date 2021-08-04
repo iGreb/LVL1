de1a86eaecac5df87acd5e99c83bf39e836283f5
igreb <igreb89@yandex.ru>
2021-08-04 23:35:41 +0500

    Задание сдал Гребёнкин Иван

---
+++ HomeWorkApp.java
@@ -0,0 +1,52 @@
+package ru.geekbrains.j1l1;
+
+public class HomeWorkApp {
+    public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+        System.out.println("Длина гипотенузы треугольника равна " + getHypotenuseLength());
+    }
+
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    public static void checkSumSign() {
+        int a = 10, b = 43, sum = a - b;
+        if (sum >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+
+    public static void printColor() {
+        int value = -50;
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (value > 0 && value <= 100) {
+            System.out.println("Жёлтый");
+        } else {
+            System.out.println("Зелёный");
+        }
+    }
+
+    public static void compareNumbers() {
+        int a = 155, b = 56;
+        if (a >= b) {
+            System.out.println("a >= b");
+        }
+        if (a < b) {
+            System.out.println("a < b");
+        }
+    }
+
+    public static double getHypotenuseLength() {
+        double a = 12.65, b = 31.47, c = Math.sqrt((a * a) + (b * b));
+        return c;
+    }
+}
